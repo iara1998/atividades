8690b2cacbffc137c99d13692bc8fd1c10eca95c
iara1998 <iara_1998@outlook.com>
2017-06-28 11:14:07 -0300

    Add files via upload

---
+++ q9 moduralizaçao1.py
@@ -0,0 +1,9 @@
+#09. Recebe um número inteiro e imprima na tela seu antecessor e o seu sucessor.
+def ant_suc( x ):  
+    b = x-1
+    c = x+1
+    return (b ,c)
+ 
+x = int(input("Digite um valor:"))
+b = ant_suc(x)
+print ("O antecessor e sucessor do numero",b)
